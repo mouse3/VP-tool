63d050d6ec2d20221b52662c3dc61dfe2036ca83
mouse3 <70779934+mouse3@users.noreply.github.com>
2021-08-21 14:08:54 +0200

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,6 @@
 import os
-os.system("sudo apt-get upgrade")
+import time
+print("si te marca una opcion de eligir Y o N siempre pon Y")
+time.sleep(5)
 os.system("sudo apt-get update")
+os.system("sudo apt-get upgrade")
